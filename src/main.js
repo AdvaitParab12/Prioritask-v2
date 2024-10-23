2cb4f429b28b5d50aed12b2a2caaed9411aa1cd4
Advait Parab <advait0012@proton.me>
2024-10-23 07:58:36 +0000

    interface completed

--- src/main.js
+++ src/main.js
@@ -1,1 +1,5 @@
 import "./index.css"
+
+const showYear = document.querySelector(".show-year");
+
+showYear.textContent = new Date().getFullYear();
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      figtree: ["Figtree", "sans-serif"],
+      libre: ["Libre", "serif"],
+    },
   },
   exports: {
     daisyui: {
-      themes: ["light","dark","cupcake"],
+      themes: ["light", "dark", "cupcake"],
     },
   },
   plugins: [require("daisyui")],
