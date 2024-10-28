2bb968d6486cf4362ea3e40d9a81e42141d5e75e
Advait Parab <advait0012@proton.me>
2024-10-28 18:30:38 +0000

    show tasks on different state

--- src/components/SingleTask.js
+++ src/components/SingleTask.js
@@ -4,7 +4,8 @@ export default function (text, isCompleted) {
   if (isCompleted) {
     label.classList.add("bg-slate-200", "rounded", "p-2");
   }
-  label.innerHTML = `<span class="label-text">${text}</span>
+  label.innerHTML = `
+  <span class="label-text">${text}</span>
   <input type="checkbox" ${
     isCompleted && "checked"
   } class="checkbox rounded-2xl" />`;
--- src/main.js
+++ src/main.js
@@ -2,32 +2,39 @@ import "./index.css";
 import SingleTask from "./components/SingleTask";
 import { titleCase } from "./utils";
 
-const showYear = document.querySelector(".show-year");
-const inputEl = document.querySelector("[data-user-input]");
 const formEl = document.querySelector("[data-form]");
+const inputEl = document.querySelector("[data-user-input]");
 const taskContainerEl = document.querySelector("[data-task-container]");
-showYear.textContent = new Date().getFullYear();
+
+const tasks = [];
 
 function renderTask() {
   taskContainerEl.innerHTML = "";
   const frag = document.createDocumentFragment();
   tasks.forEach((task) => {
-    frag.appendChild(SingleTask(task.text, task.isCompleted));
+    frag.appendChild(SingleTask(task.task, task.isCompleted));
   });
   taskContainerEl.appendChild(frag);
 }
 
-const tasks = [];
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!inputEl.value) return;
+
   const newTask = {
     task: titleCase(inputEl.value),
-    isCompleted: false,
+    isCompleted: true,
     id: tasks.length,
   };
+
   tasks.unshift(newTask);
+
   renderTask();
 
+  console.log(tasks);
+  
   inputEl.value = "";
 });
+
+const showYear = document.querySelector(".show-year");
+showYear.textContent = new Date().getFullYear();
